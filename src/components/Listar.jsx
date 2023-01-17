1eb063b310aa6ee20f6c16c60f0f40a04e8c419e
Erika Juliana Ruiz <erikaruiz06@hotmail.com>
2023-01-16 19:56:40 -0500

    login-react1

--- src/components/Listar.jsx
+++ src/components/Listar.jsx
@@ -1,40 +1,90 @@
-import React from "react";
-export const Listar =() =>{
-    return(
-        
-      <div className="bg-light" style={{marginTop:20, padding:20}}>
+import React, { useState, useEffect } from 'react'
 
-      <div className="h3">
-        Listado De Registro De Pinturas
-      </div>
+export const Listar = () => {
 
-      <div className="table-responsive">
-        
+  const obtenerRegistros = () => {
+    var datos = localStorage.getItem("registroslogin");
+    if(datos){
+      return JSON.parse(datos);
+    }else{
+      return [];
+    }
+  }
 
 
-        <>
-          <table className="table table-bordered table-hover" style={{marginTop:12}}>
-              <thead className="text-center" style={{background:"lightgray"}}>
-                  <tr>
-                      <th>#</th>
-                      <th>Titulo</th>
-                      <th>Estilo</th>
-                      <th>Tecnica</th>
-                      <th>Precio</th>
-                      <th>X</th>
-                  </tr>
-              </thead>
-              <tbody className="text-center align-baseline">
-                  
-              </tbody>
-          </table>
-        </> 
-        
-        
+  const [registroslogin, setRegistrosLogin] = useState(obtenerRegistros());
 
-      </div>
-   
+  const botonEliminar = (miIndex) => {
+    if(window.confirm("¿Está Seguro De Querer Eliminar El Registro?")){
+      var registrosFiltrados = registroslogin.filter((e, index) => {
+          return miIndex !== index
+      });
+      setRegistrosLogin(registrosFiltrados);
+    }
+  }
+
+
+  useEffect(() => {
+    localStorage.setItem("registroslogin", JSON.stringify(registroslogin))
+  }, [registroslogin])
+
+
+
+  return (
+    
+
+    
+    <div className="bg-light" style={{marginTop:20, padding:20}}>
+
+    <div className="h3">
+      Listado De Registro De Pinturas
     </div>
 
-    )
+    <div className="table-responsive">
+      
+      { registroslogin.length > 0 ? 
+
+      <>
+        <table className="table table-bordered table-hover" style={{marginTop:12}}>
+            <thead className="text-center" style={{background:"lightgray"}}>
+                <tr>
+                    <th>#</th>
+                    <th>Titulo</th>
+                    <th>Estilo</th>
+                    <th>Tecnica</th>
+                    <th>Precio</th>
+                    <th>X</th>
+                </tr>
+            </thead>
+            <tbody className="text-center align-baseline">
+                {
+                  registroslogin.map((x, index) => (
+                    <tr key={index}>
+                      <th>{ index+1 }</th>
+                      <td>{ x.titulo }</td>
+                      <td>{ x.estilo }</td>
+                      <td>{ x.tecnica }</td>
+                      <td>{ x.precio }</td>
+                      <td className="text-center">
+                        <button className="btn btn-outline-danger" onClick={()=>botonEliminar(index)}>
+                          <i class="bi bi-trash3-fill"></i>
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                }
+            </tbody>
+        </table>
+      </> 
+      
+      : <p className='h5' style={{color:"red"}}>"No Hay Registros Para Listar"</p>
+      }
+      
+    </div>
+ 
+  </div>
+
+
+
+  )
 }
